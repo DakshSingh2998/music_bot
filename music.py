5cceb6a484fd69b1f4dbfb4a4aa22167555cb4f0
DakshSingh2998 <31823365+DakshSingh2998@users.noreply.github.com>
2021-07-05 08:20:09 +0530

    Update music.py

--- music.py
+++ music.py
@@ -27,7 +27,7 @@ ytdlopts = {
 'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
-        'preferredquality': '64',
+        'preferredquality': '32',
     }],
     
 }
