afba154a0f33b02663f17df950bcc8774d775841
DakshSingh2998 <31823365+DakshSingh2998@users.noreply.github.com>
2021-07-07 02:34:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -166,7 +166,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
             global timeestamp
         
         ffmpegopts = {
-        'before_options': f'-vn -nostdin -ss {timeestamp} -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 4000 -stimeout 10000000 -ab 64' ,
+        'before_options': f'-vn -nostdin -ss {timeestamp} -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 4000 -stimeout -ab 64' ,
         'options': f'-vn'
         }
 
@@ -187,7 +187,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         data = await loop.run_in_executor(None, to_run)
         global timeestamp
         ffmpegopts = {
-        'before_options': f'-vn -nostdin -ss {timeestamp} -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 4000 -stimeout 10000000 -ab 64' ,
+        'before_options': f'-vn -nostdin -ss {timeestamp} -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 4000 -stimeout -ab 64' ,
         'options': f'-vn'
         }
 
