144ccc29521898f41d53a538bd8fa871f2035f40
DakshSingh2998 <31823365+DakshSingh2998@users.noreply.github.com>
2021-08-29 07:37:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1710,7 +1710,7 @@ async def on_reaction_add(reaction, user):
     channel_id = channell.id
     player=get_player(ctx)
     if reaction.message.channel.id == channel_id:
-      player.cttx=ctx
+      #player.cttx=ctx
       await ctx.send(f'**`{user}`**: Reacted!')
       #print(reaction.emoji)
       if str(reaction.emoji) =='⏯️':
